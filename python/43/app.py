6a8986a1973b805a5902fa84bd2a408d2ee285b4
slubowsky-course <slubowsky.course@gmail.com>
2021-05-02 22:57:50 -0400

    class 43

--- python/43/app.py
+++ python/43/app.py
@@ -1,3 +1,18 @@
 print("Hello World")
 x = 5
-print(x)
+print(x, type(x))
+x = 'Hello'
+print(x, type(x))
+y = True
+print(y, type(y))
+z = 1.2
+print(z, type(z))
+# this is a comment
+
+my_long_variable_name = 5
+my_very_long_int = 576743746346378456354365437543875643785643785465478356437856435456346543753
+print(my_very_long_int, type(my_very_long_int))
+
+print(id(my_very_long_int))
+my_very_long_int += 1
+print(id(my_very_long_int))
