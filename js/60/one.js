60e6074b9b495b3dd3af64a5c61311cf3c93d51a
slubowsky <270589+slubowsky@users.noreply.github.com>
2025-07-20 22:45:17 -0400

    class 60

---
+++ js/60/one.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log("Hello World!");
+
+var x = 5;
+console.log(x, typeof (x));
+x = 'hello';
+console.log(x, typeof (x));
+
+let name = 'Donald';
+// 5000 lines later
+//nam = 'JD';
+
+//console.log(name);
+
+let z = 10;
+z = 12;
+const PI = 3.14;
+//PI = 3.145;
+
+/* multiple line
+comment */
+
+for(let i = 0; i < 10; i++) {
+  console.log(i);
+}
